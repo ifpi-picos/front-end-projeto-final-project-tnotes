1957dd4ad1ea87976a3629ea06ea04abfd0fd9db
WalberAlencar <53355785+WalberAlencar@users.noreply.github.com>
2020-01-16 21:37:42 -0300

    'alteracao_na_dashboard'

--- js/main.js
+++ js/main.js
@@ -1,18 +1,51 @@
 const inputs = document.querySelectorAll(".input");
 
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
+function addcl() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
 }
 
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
+function remcl() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
 }
 
 inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
-});
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+})
+let dropdown = document.getElementsByClassName("dropdown-btn");
+let i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+let sbt = document.getElementById("myBtn2")
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+sbt.onclick = function() {
+    // termina dps
+    modal.style.display = "none";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
