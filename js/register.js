c38b97ea5469a41c0628c06f16d8dd452852d94e
ulisseslimaa <ulisses.lima741@gmail.com>
2020-01-22 13:25:11 -0300

    validacao campos cadastro

---
+++ js/register.js
@@ -0,0 +1,42 @@
+const mostrar = document.querySelector(".success");
+const mostarErr = document.querySelector('.error');
+const cadastrar = document.querySelector('#enviar')
+
+cadastrar.addEventListener('click', () => {
+    const url = 'http://localhost:3000/api/users'
+    const user = {
+        name: '',
+        email: '',
+        password: '',
+    };
+    user.name = document.querySelector('#name').value;
+    user.email = document.querySelector('#email').value;
+    user.password = document.querySelector("#senha").value;
+
+    console.log(user)
+    const option = {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    }
+
+    fetch(url, option)
+        .then((response) => {
+            if (response.ok) {
+                mostrar.classList.add('mostrar');
+                setTimeout(() => {
+                    mostrar.classList.remove('mostrar')
+                }, 2000)
+            } else {
+
+                mostarErr.classList.add('mostrar');
+                setTimeout(() => {
+                    mostarErr.classList.remove('mostrar')
+                }, 2000)
+            }
+
+        })
+
+})
